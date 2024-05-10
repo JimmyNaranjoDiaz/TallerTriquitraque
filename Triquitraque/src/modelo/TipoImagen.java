4dbf653d28d450cea66066d33112b86223018613
JimmyNaranjoDiaz <160796853+JimmyNaranjoDiaz@users.noreply.github.com>
2024-05-10 01:47:56 -0500

    Add files via upload

---
+++ Triquitraque/src/modelo/TipoImagen.java
@@ -0,0 +1,10 @@
+
+package modelo;
+
+/**
+ *
+ * @author Jimmy Naranjo
+ */
+public enum TipoImagen {
+    EQUIS,CIRCULO,LINEA1,LINEA2,LINEA3,LINEA4,LINEA5,LINEA6,LINEA7,LINEA8,EMPATE
+}
---
+++ Triquitraque/src/principal/Principal.java
@@ -0,0 +1,17 @@
+
+package principal;
+
+import vista.InterfazInicio;
+
+/**
+ *
+ * @author Jimmy Naranjo
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        InterfazInicio formInicio = new InterfazInicio();
+        formInicio.setVisible(true);
+    }
+    
+}
